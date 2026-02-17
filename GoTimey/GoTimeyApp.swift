9ad08115ba13ac6199c59c8615f26e45f46f9a5a
Jake Rappa <rappajake@gmail.com>
2026-02-17 12:27:53 -0500

    Initial Commit

---
+++ GoTimey/GoTimeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTimeyApp.swift
+//  GoTimey
+//
+//  Created by Jake on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct GoTimeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoTimeyTests/GoTimeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoTimeyTests.swift
+//  GoTimeyTests
+//
+//  Created by Jake on 2/17/26.
+//
+
+import Testing
+@testable import GoTimey
+
+struct GoTimeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
